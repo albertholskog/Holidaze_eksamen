a551a630a0eef67303788aaa8442fb3a153f5284
albertholskog <albert_holskog@live.no>
2023-05-05 10:30:54 +0200

    hooks fix ?

--- src/pages/VenueList.jsx
+++ src/pages/VenueList.jsx
@@ -1,4 +1,4 @@
-import useApi from "../Hooks/useApi";
+import useApi from "../hooks/useApi";
 import CardVenue from "../components/Card";
 import { Grid } from "@mui/material";
 import noImage from "../image/noImage.jpg"
